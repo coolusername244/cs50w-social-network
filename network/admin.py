c0d29ccf60cc1ddbbc7e9dba42adc542bcb20943
Lee <leesheppard2404@gmail.com>
2023-01-14 14:00:38 +0100

    added user admin view

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username"
+    )
+
+
+admin.site.register(User, UserAdmin)
